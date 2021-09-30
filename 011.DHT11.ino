8af74418c3b25bcb8a258eb97be55d7a7ed242a5
Mina Lee <wwft09@naver.com>
2021-09-30 22:04:07 +0900

    011. DHT11(humidity,temperature)

---
+++ 011.DHT11.ino
@@ -0,0 +1,26 @@
+#include <DHT.h>
+#include <LiquidCrystal.h>
+#define DHTPIN 8
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+LiquidCrystal lcd(12,11,5,4,3,2);
+
+void setup() {
+  lcd.begin(16,2);
+}
+
+void loop() {
+  delay(2000);
+  int h = dht.readHumidity();
+  int t = dht.readTemperature();
+  lcd.setCursor(0,0);
+  lcd.print("Humi: ");
+  lcd.print(h);
+  lcd.print(" %");
+
+  lcd.setCursor(0,1);
+  lcd.print("Temp: ");
+  lcd.print(t);
+  lcd.print(" C");
+}
